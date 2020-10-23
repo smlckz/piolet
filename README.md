55df3e8523af0b804082f42123a98e3c6e0a9083
Sudipto Mallick <smallick.dev@gmail.com>
2020-10-23 14:09:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# piolet
+
+*piolet* is a Tcl like language interpreter implemented in C89.
+
---
+++ piolet.c
@@ -0,0 +1,6 @@
+/* piolet: a lightweight Tcl like language interpreter written in C89 */
+
+int main(void) {
+    return 0;
+}
+
